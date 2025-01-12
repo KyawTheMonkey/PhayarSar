fdad504ac7bef99d8d931a6f7d8600503999c10a
Kyaw Monkey <kyaw.codes@gmail.com>
2025-01-12 15:13:53 +0700

    chore: ui polish for weekly chart

--- PhayarSar/Features/Home/Views/DailyPrayingTimeChartView.swift
+++ PhayarSar/Features/Home/Views/DailyPrayingTimeChartView.swift
@@ -9,28 +9,80 @@ import SwiftUI
 import Charts
 import CoreData
 
+struct Line: Shape {
+  func path(in rect: CGRect) -> Path {
+    Path { path in
+      path.move(to: .init(x: rect.minX, y: rect.midY))
+      path.addLine(to: .init(x: rect.maxX, y: rect.midY))
+    }
+  }
+}
+
 struct DailyPrayingTimeChartView: View {
   private let calendar = Calendar.current
   private let stack = CoreDataStack.shared
   @State private var isFetching = false
   @State private var weekData = [DailyPrayingTimeVO]()
+  @State private var totalSeconds = 0
+  @State private var showAverage = false
   @EnvironmentObject private var preferences: UserPreferences
-
+  
   var body: some View {
-    ZStack {
-      if isFetching {
-        Text("Calclating...⌛")
-      } else {
-        Chart {
-          ForEach(weekData) { data in
-            BarMark(x: .value("Day of Week", data.date.toStringWith(.EE)),
-                    y: .value("Seconds", data.durationInSeconds))
-            .foregroundStyle(preferences.accentColor.color)
+    VStack(spacing: 12) {
+      HStack {
+        VStack(alignment: .leading, spacing: 0) {
+          LocalizedText(.x_sec, args: ["\(totalSeconds)"])
+            .font(.dmSerif(28))
+          LocalizedText(.within_this_week)
+            .font(.qsR(10))
+        }
+        
+        Spacer()
+        
+        Checkbox(title: "Show Avg.", value: $showAverage)
+      }
+      ZStack {
+        if isFetching {
+          Text("Calclating...⌛")
+        } else {
+          Chart {
+            ForEach(weekData) { data in
+              BarMark(
+                x: .value(
+                  "Day of Week",
+                  DaysOfWeek(
+                    rawValue: data.date.toStringWith(.EE).lowercased()
+                  )?.shortName(appLang: preferences.appLang) ?? ""
+                ),
+                y: .value("Seconds", data.durationInSeconds)
+              )
+              .foregroundStyle(
+                by: .value("Day of Week", DaysOfWeek(rawValue: data.date.toStringWith(.EE).lowercased())?.shortName(appLang: preferences.appLang) ?? "")
+              )
+            }
+            
+            RuleMark(y: .value("Average", Double(totalSeconds) / 7))
+              .lineStyle(.init(lineWidth: 1.5, dash: [5], dashPhase: 3))
+              .foregroundStyle(.blue)
+              .annotation(alignment: .trailing) {
+                Text("\(totalSeconds / 7) sec")
+                  .foregroundStyle(.black)
+                  .font(.qsB(8))
+                  .padding(.horizontal, 8)
+                  .padding(.vertical, 4)
+                  .background {
+                    RoundedRectangle(cornerRadius: 4)
+                      .fill(.white)
+                      .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 0)
+                  }
+                  .opacity(showAverage ? 1 : 0)
+              }
+              .opacity(showAverage ? 1 : 0)
           }
         }
       }
+      .frame(height: 350)
     }
-    .frame(height: 360)
     .task {
       await fetch()
     }
@@ -73,11 +125,17 @@ struct DailyPrayingTimeChartView: View {
           sortedArray.append(.init(date: date, durationInSeconds: 0))
         }
       }
-
+      
       await MainActor.run {
         weekData = sortedArray
+        totalSeconds = Int(sortedArray.reduce(0) { $0 + $1.durationInSeconds })
         isFetching = false
       }
     }
   }
+}
+
+#Preview {
+  DailyPrayingTimeChartView()
+    .previewEnvironment()
 }
