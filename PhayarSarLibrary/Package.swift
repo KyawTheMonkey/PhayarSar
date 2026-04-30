7791c355f59e59340aa7d7d9211104c5ace18c5d
Kyaw Monkey <kyaw.codes@gmail.com>
2026-04-30 10:19:34 +0700

    downgrade the minimum supported version to iOS 16 for SPM

--- PhayarSarLibrary/Package.swift
+++ PhayarSarLibrary/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "PhayarSarLibrary",
-  platforms: [.iOS(.v18), .macOS(.v15)],
+  platforms: [.iOS(.v16), .macOS(.v13)],
   products: [
     .library(
       name: "UtilKit",
