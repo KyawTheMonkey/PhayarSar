eaee54455211cf95d8fead3dc4c2838752c2d023
Kyaw Monkey <kyaw.codes@gmail.com>
2026-08-10 13:09:32 +0700

    added pryaerkit

--- PhayarSarLibrary/Sources/PrayersKit/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  PhayarSarLibrary
-//
-//  Created by Kyaw Zay Ya Lin Tun on 09/08/2026.
-//
-
-import Foundation
---
+++ PhayarSarLibrary/Sources/PrayersKit/Prayers.swift
@@ -0,0 +1,28 @@
+import Foundation
+import LocalisationKit
+
+enum PrayerCategory: Int, Hashable, CaseIterable {
+  case precepts
+  case virtues
+  case parittas
+  case protection
+  case discourses
+  case metta
+  
+  var displayText: String {
+    switch self {
+    case .precepts:
+      return L10n.precepts
+    case .virtues:
+      return L10n.virtues
+    case .parittas:
+      return L10n.parittas
+    case .protection:
+      return L10n.protective
+    case .discourses:
+      return L10n.discourses
+    case .metta:
+      return L10n.metta
+    }
+  }
+}
